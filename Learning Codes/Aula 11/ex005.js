96521e609f60d09c63309757b328c1029512f3c7
Leao-E <eemanuelleao@gmail.com>
2020-02-06 16:01:56 -0300

    Codes from new lesson

---
+++ Learning Codes/Aula 11/ex005.js
@@ -0,0 +1,14 @@
+/*Condição Simples*/
+var vel = 6
+console.log(`A velocidade é ${vel}km/h`)
+if (vel > 60){
+    console.log(`tá rápido demais: MULTADO`)
+}
+console.log(`dirija sempre acordado (e com sinto de segurança)`)
+/*Condição Composta*/
+var pais = "Braia"
+if(pais == "Brasil"){
+    console.log("Brasileiro")
+}else{
+    console.log("Extrangeiro")
+}
